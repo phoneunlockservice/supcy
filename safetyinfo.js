125f9bfa3bc1ecca282bc1aff4afea4e73019331
phone unlock service <133379062+phoneunlockservice@users.noreply.github.com>
2023-05-12 22:50:51 +0100

    Add files via upload

---
+++ safetyinfo.js
@@ -0,0 +1,7 @@
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+   alert("Your email address won't be shared or published on any other website. see Privacy policy");
+} 
+
+)
